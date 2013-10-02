c1ed94486940ef49ba4553a7e861dd7ec988edfe
cameron hill <cameron.hill@gmail.com>
2013-10-01 23:35:30 -0700

    Nest children under spouses in d3 tree. Add relationship form slightly improved.

--- app/scripts/controllers/people.js
+++ app/scripts/controllers/people.js
@@ -37,7 +37,68 @@ window.angular.module('ngl2.controllers.people', [])
 				});
 			};
 
+			$scope.updateRelation = function () {
+				var params = {
+					_id: $routeParams.personId,
+					treeId: $routeParams.treeId,
+					person: {}
+				}
 
+				params.person[inversePluralize($routeParams.action)] = [$scope.relation];
+
+				console.log(params);
+				
+				new People(params).$update(function (response) {
+					$location.path('trees/' + $routeParams.treeId);
+				});
+			};
+
+			$scope.prepareRelatives = function () {
+				//$scope._people = Trees.getPeople();
+				console.log($routeParams, $scope._people);
+				var root = $scope.root = $scope._people[$routeParams.personId],
+						i = 0, j = 0;
+
+				$scope.action = $routeParams.action;
+				$scope.suggestedRelatives = [];
+
+				// TBD: suggestedRelatives should only return unique values
+				// 			and values not already related to root
+				switch ($scope.action) {
+				case 'child':
+					// create a pool of potential children from existing spouse's children
+					for (i in root.spouse_ids) {
+						var spouse = $scope._people[root.spouse_ids[i]];
+
+						for (j in spouse.child_ids) {
+							$scope.suggestedRelatives.push($scope._people[spouse.child_ids[j]]);	
+						}
+					}
+					break;
+				case 'spouse':
+					// create a pool of potential spouses from existing children's parents?
+					for (i in root.child_ids) {
+						var child = $scope._people[root.child_ids[i]];
+
+						for (j in child.parent_ids) {
+							$scope.suggestedRelatives.push($scope._people[child.parent_ids[j]]);	
+						}
+					}
+					break;
+				case 'parent':
+					// create a pool of potential parents from existing parent's spouses
+					for (i in root.parent_ids) {
+						var parent = $scope._people[root.parent_ids[i]];
+
+						for (j in parent.spouse_ids) {
+							$scope.suggestedRelatives.push($scope._people[parent.spouse_ids[j]]);	
+						}
+					}
+					break;
+				}
+
+				console.log('suggestedRelatives', $scope.suggestedRelatives);
+			};
 
 			$scope.showForm = function (options) {
 				$location
@@ -54,9 +115,9 @@ window.angular.module('ngl2.controllers.people', [])
 
 				console.log('action', $routeParams.action);
 
-				if($routeParams.action) {
+				if ($routeParams.action) {
 					
-					switch($routeParams.action) {
+					switch ($routeParams.action) {
 					case 'child':
 						params.person.children = [$routeParams.personId];
 						break;
@@ -84,4 +145,15 @@ window.angular.module('ngl2.controllers.people', [])
 					$location.path('trees/' + $routeParams.treeId);
 				});
 			};
+
+			function inversePluralize (option) {
+				switch (option) {
+				case 'child':
+					return 'parents';
+				case 'parent':
+					return 'children';
+				case 'spouse':
+					return 'spouses';
+				}
+			}
 		}]);
--- app/scripts/directives/tree.js
+++ app/scripts/directives/tree.js
@@ -15,15 +15,31 @@ window.angular.module('ngl2.directives.tree', [])
 				// build node list
 				nodeData.children = [];
 				nodeData.spouses = [];
-				for(var i in nodeData.spouse_ids) {
-					var id = nodeData.spouse_ids[i];
-					nodeData.spouses.push(angular.copy(_people[id]));
+				for (var i in nodeData.spouse_ids) {
+					var id = nodeData.spouse_ids[i],
+							spouse = angular.copy(_people[id]),
+							children = [];
+
+					for (var j in spouse.child_ids) {
+						var childId = spouse.child_ids[j];
+
+						if (nodeData.child_ids.indexOf(childId) > -1) {
+							children.push(angular.copy(_people[childId]));
+						}
+					}
+
+					if(children.length) {
+						spouse.children = children;
+					}
+
+					nodeData.spouses.push(spouse);
 				}
 				// TODO: intersect spouse children to nest under this spouse
-				for(var i in nodeData.child_ids) {
-					var id = nodeData.child_ids[i];
-					nodeData.children.push(angular.copy(_people[id]));
-				}
+				// for(var i in nodeData.child_ids) {
+				// 	var id = nodeData.child_ids[i];
+
+				// 	nodeData.children.push(angular.copy(_people[id]));
+				// }
 
 	      var vis = d3.select(elem[0])
 		      .append('svg:g')
@@ -50,10 +66,6 @@ window.angular.module('ngl2.directives.tree', [])
 	      var nodes = tree.nodes(nodeData);
 	      var links = tree.links(nodes);
 	 
-	      // console.log(nodeData);
-	      // console.log(nodes);
-	      // console.log(links);
-	 
 	      var link = vis.selectAll('pathlink')
 		      .data(links.filter(function(d) {
 						// links where target is in source.child list
@@ -70,6 +82,7 @@ window.angular.module('ngl2.directives.tree', [])
 		      }))
 		      .enter().append('svg:path')
 		      .attr('class', 'link spouse')
+		      .attr('stroke-dasharray', '4,4')
 		      .attr('d', diagonal);
  
 	      var node = vis.selectAll('g.node')
@@ -92,7 +105,7 @@ window.angular.module('ngl2.directives.tree', [])
 	      node.append('svg:text')
 	      //.attr('text-anchor', function(d) { return d.children ? 'end' : 'start'; })
 	      .attr('transform', function(d) {
-					return 'rotate(-30)translate(6,2)';
+					return 'rotate(-45)translate(6,2)';
 				})
 	      .text(function(d) { return d.birth_name; });
 			}

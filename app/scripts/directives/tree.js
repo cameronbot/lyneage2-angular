a35530e1467f85dacb067954c5d8ed59ab10f877
cameron hill <cameron.hill@gmail.com>
2013-10-06 19:10:17 -0700

    Clean up d3 tree.js directive

--- app/scripts/directives/tree.js
+++ app/scripts/directives/tree.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.angular.module('ngl2.directives.tree', [])
-	.directive('tree', ['Trees', '$rootScope', function (Trees, $rootScope) {
+	.directive('tree', ['Trees', function (Trees) {
 		return {
 			restrict: 'A',
 			scope: {
@@ -33,32 +33,37 @@ window.angular.module('ngl2.directives.tree', [])
 				var diagonal = d3.svg.diagonal()
 							// change x and y (for the left to right tree)
 							.projection(function(d) { return [d.y, d.x]; });
-			 
-				scope.$watch('person', function(newValue, oldValue) {
-          if (newValue) {
-            console.log("I see a data change!");
+			
+				var drawTree = function(newValue, oldValue) {
+					var i = 0, j = 0,
+							id, spouse, children;
 
+          if (newValue) {
+            // clear existint chart
             vis.remove();
 
             vis = d3.select(elem[0])
 				      .append('svg:g')
-				      .attr('transform', 'translate(20, 50)'); // shift everything to accomodate diagonal labels
+				      // shift everything to accomodate diagonal labels
+				      .attr('transform', 'translate(20, 50)');
 				 
-            nodeData = angular.copy(scope.person);
-
 						// build node list
+            nodeData = angular.copy(scope.person);
 						nodeData.children = [];
 						nodeData.spouses = [];
-						for (var i in nodeData.spouse_ids) {
-							var id = nodeData.spouse_ids[i],
-									spouse = angular.copy(_people[id]),
-									children = [];
 
-							for (var j in spouse.child_ids) {
-								var childId = spouse.child_ids[j];
+						// loop over spouses
+						for (i in nodeData.spouse_ids) {
+							id = nodeData.spouse_ids[i];
+							spouse = angular.copy(_people[id]);
+							children = [];
 
-								if (nodeData.child_ids.indexOf(childId) > -1) {
-									children.push(angular.copy(_people[childId]));
+							// find children of spouse and person (intersection)
+							for (j in spouse.child_ids) {
+								id = spouse.child_ids[j];
+
+								if (nodeData.child_ids.indexOf(id) > -1) {
+									children.push(angular.copy(_people[id]));
 								}
 							}
 
@@ -67,22 +72,13 @@ window.angular.module('ngl2.directives.tree', [])
 							}
 
 							nodeData.spouses.push(spouse);
-						}
-						// TODO: intersect spouse children to nest under this spouse
-						// for(var i in nodeData.child_ids) {
-						// 	var id = nodeData.child_ids[i];
+						} // end spouse loop
 
-						// 	nodeData.children.push(angular.copy(_people[id]));
-						// }
-
-			      
-			      
-			 
-			      
 			      var nodes = tree.nodes(nodeData);
 			      var links = tree.links(nodes);
 			 
-			      var link = vis.selectAll('pathlink')
+			      //var link =
+			      vis.selectAll('pathlink')
 				      .data(links.filter(function(d) {
 								// links where target is in source.child list
 								return d.source.child_ids.indexOf(d.target._id) > -1;
@@ -91,7 +87,8 @@ window.angular.module('ngl2.directives.tree', [])
 				      .attr('class', 'link')
 				      .attr('d', diagonal);
 		 
-						var spouseLink = vis.selectAll('pathlink')
+						//var spouseLink = 
+						vis.selectAll('pathlink')
 				      .data(links.filter(function(d) {
 								// links where target is in source.spouse list
 								return d.source.spouse_ids.indexOf(d.target._id) > -1;
@@ -117,16 +114,17 @@ window.angular.module('ngl2.directives.tree', [])
 				
 			      node.append('svg:circle').attr('r', 3.5);
 			 
-			      // place the name atribute left or right depending if children
 			      node.append('svg:text')
-			      	//.attr('text-anchor', function(d) { return d.children ? 'end' : 'start'; })
-				      .attr('transform', function(d) {
+							.text(function(d) { return d.birth_name; })
+							.attr('transform', function(d) {
+								// rotate label
 								return 'rotate(-45)translate(6,2)';
-							})
-			      	.text(function(d) { return d.birth_name; });
+							});
 
           }
-        }, true);
+        };
+
+				scope.$watch('person', drawTree, true);
 			}
 		};
 	}]);

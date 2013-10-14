b89140cd168ebaff733664bcb71362c7dcf98665
cameron hill <cameron.hill@gmail.com>
2013-10-14 16:47:36 -0700

    Added a separate API path constant because of bug in  that ignores escaped port numbers

--- app/scripts/services/global.js
+++ app/scripts/services/global.js
@@ -1,4 +1,5 @@
 'use strict';
 
 window.angular.module('ngl2.services.global', [])
-	.constant('API_ROOT', 'http://localhost\\:3000/api/v1');
+	.constant('API_ROOT', 'http://localhost\\:3000/api/v1')
+	.constant('HTTP_API_ROOT', 'http://localhost:3000/api/v1');
